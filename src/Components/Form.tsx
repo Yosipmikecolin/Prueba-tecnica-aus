4944cd354d0ce5c13f1593784a6a740144e6f322
Yosip Mike Colin <colinparrado@gmail.com>
2022-04-09 14:38:56 -0500

    base code

---
+++ src/Components/Form.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+
+function Form (){
+
+
+    return(
+<FormLogin>
+<h1>Form</h1>
+
+</FormLogin>
+       
+    )
+
+}
+
+
+const FormLogin = styled.form`
+width:300px;
+padding:20px;
+background-color:orange;
+
+`;
+
+export default Form;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,44 @@
+import {NavLink} from "react-router-dom";
+import styled from "styled-components";
+
+function Header(){
+
+
+    return (
+
+        <Nav>
+            <ul>
+                <li><NavLink to="/login">Login</NavLink></li>
+                <li><NavLink to="/elementos">Elementos</NavLink></li>
+            </ul>
+        </Nav>
+    )
+
+}
+
+
+const Nav = styled.nav`
+width:100%;
+padding:15px;
+background-color:#1B1A17;
+display:flex;
+justify-content:center;
+font-size:20px;
+
+
+a.active{
+color:#332FD0;
+}
+
+ul > li{
+    display:inline-block;
+    margin:0px 10px;
+}
+
+a{
+    color:#fff;
+}
+
+`;
+
+export default Header;
